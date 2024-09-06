baa7692c21cbe5ea6e585124affe0aa148e36e79
DS0708 <jonum12312@gmail.com>
2024-09-06 13:12:09 +0900

    add mongo

--- src/main/java/com/ktb/ktbhackathonbe/config/MongoConfig.java
+++ src/main/java/com/ktb/ktbhackathonbe/config/MongoConfig.java
@@ -19,19 +19,19 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
-@EnableMongoRepositories(basePackageClasses = ImageRepository.class, mongoTemplateRef = "MongoTemplate")
+@EnableMongoRepositories(basePackageClasses = ImageRepository.class, mongoTemplateRef = "ImageMongoTemplate")
 @EnableConfigurationProperties
 public class MongoConfig {
 
-    @Bean(name = "MongoProperties")
-    @ConfigurationProperties(prefix = "mongodb")
+    @Bean(name = "ImageMongoProperties")
+    @ConfigurationProperties(prefix = "mongodb.image")
     @Primary
     public MongoProperties alphavantageProperties() {
         return new MongoProperties();
     }
 
-    @Bean(name = "MongoClient")
-    public MongoClient mongoClient(@Qualifier("MongoProperties") MongoProperties mongoProperties) {
+    @Bean(name = "ImageMongoClient")
+    public MongoClient mongoClient(@Qualifier("ImageMongoProperties") MongoProperties mongoProperties) {
         // MongoDB URI 생성
         String uri = "mongodb://" + mongoProperties.getHost() + ":" + mongoProperties.getPort();
 
@@ -42,16 +42,16 @@ public class MongoConfig {
     }
 
     @Primary
-    @Bean(name = "MongoDBFactory")
+    @Bean(name = "ImageMongoDBFactory")
     public MongoDatabaseFactory mongoDatabaseFactory(
-            @Qualifier("MongoClient") MongoClient mongoClient,
-            @Qualifier("MongoProperties") MongoProperties mongoProperties) {
+            @Qualifier("ImageMongoClient") MongoClient mongoClient,
+            @Qualifier("ImageMongoProperties") MongoProperties mongoProperties) {
         return new SimpleMongoClientDatabaseFactory(mongoClient, mongoProperties.getDatabase());
     }
 
     @Primary
-    @Bean(name = "MongoTemplate")
-    public MongoTemplate mongoTemplate(@Qualifier("MongoDBFactory") MongoDatabaseFactory mongoDatabaseFactory) {
+    @Bean(name = "ImageMongoTemplate")
+    public MongoTemplate mongoTemplate(@Qualifier("ImageMongoDBFactory") MongoDatabaseFactory mongoDatabaseFactory) {
         return new MongoTemplate(mongoDatabaseFactory);
     }
 }
